e2d48cb6cd3607275dd9ea24e4d5b4cc32dd7817
Giovanni Francesco Capalbo <giovanni@therp.nl>
2020-05-14 18:33:27 +0200

    FIX

--- project_parent/migrations/12.0.6.0.0/post-migration.py
+++ project_parent/migrations/12.0.6.0.0/post-migration.py
@@ -8,7 +8,4 @@ _logger = logging.getLogger(__name__)
 def migrate(cr, version):
     _logger.debug("Create parent_id columns with values")
     cr.execute("""
-        ALTER TABLE project_project ADD project_parent_id integer
-        """)
-    cr.execute("""
         UPDATE project_project set  project_parent_id = parent_id """)
